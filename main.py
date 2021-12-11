ead95f43cb0bf2b5d0a586644b3d8b44df714a8b
stevef <stevefrazee123@gmail.com>
2021-12-11 17:24:58 -0500

    remove unused function / import

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-from operator import itemgetter
-
 from menu import RunMenu
 from war import War
-
-def DealCards(deck, players, cards_per_player=None):
-    num_players = len(players)
-    if cards_per_player:
-        for i in range(cards_per_player * num_players):
-            card = deck.GetTopCard()
-            player = players[i % num_players]
-            player.AddCard(card)
-    else:
-        i = 0;
-        while(len(deck.cards) > 0):
-            card = deck.GetTopCard()
-            player = players[i % num_players]
-            player.AddCard(card)
-            i += 1
 
 def main():
     games = [War()]
